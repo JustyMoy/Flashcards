85be26ac21efe23145df3d6d558f7b9d6b08d69e
Justin Moy <moy.justin94@gmail.com>
2023-02-11 16:14:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+import './app.css'
+// import axios from 'axios'
+// for api use and want to import api, add 'useEffect' hook so the api works as soon as page load
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+<FlashcardList flashcards={flashcards} /> 
     </div>
-  );
+);
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'what is plus',
+    answer: '4',
+    options: [
+      ' -1',
+      ' -2',
+      ' 4',
+  
+    ]
+  },
+
+  {
+  id: 2,
+  question: 'what is pie',
+  answer: '3.14',
+  options: [
+    ' 1',
+    ' 2',
+    ' 3.14',
+
+  ]
+},
+{
+id: 3,
+question: '2 + 2',
+answer: '4',
+options: [
+  'Answer 5',
+  'Answer 2',
+  'Answer 4',
+
+]
+}
+]
 
 export default App;
